5a8cd900125f320e1fa8226dc62b7a22e9ce98df
Christopher Anderson <chris@nullcode.org>
2015-11-04 11:44:43 -0800

    [env] Speed up env.bash

--- env.bash
+++ env.bash
@@ -5,14 +5,20 @@ OS=`uname`
 HOSTARCH=`uname -m`
 DIR=`dirname "$BASH_SOURCE"`
 SELF=`basename "$BASH_SOURCE"`
+# OSX readlink doesn't support -f, but greadlink is the gnu version
+# that Linux uses
+READLINK=`which greadlink || which readlink`
 
 if [ "${BASH_SOURCE[0]}" = "${0}" ]; then
     echo "$SELF must be sourced, not executed"
     exit 1
 fi
 
-for p in `find $DIR -type d -wholename "$DIR/*-$OS-$HOSTARCH/bin"`;
+for p in `ls -d $DIR/*-$OS-$HOSTARCH | grep -v build`;
 do
-    echo "added $p";
-    export PATH=$p:$PATH;
+    if [ -d $p/bin ]; then
+        ABS_PATH=`$READLINK -f $p`
+        echo "added $ABS_PATH";
+        export PATH=$ABS_PATH/bin:$PATH;
+    fi
 done
